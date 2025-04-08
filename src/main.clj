a5989b898c9f5809a038b94b87c89bf916a4ed86
ajo34 <addavadda@gmail.com>
2025-04-08 15:39:53 +0200

    initial commit

---
+++ src/main.clj
@@ -0,0 +1,15 @@
+(ns main)
+(require '[next.jdbc :as jdbc])
+(def db {:dbtype "h2" :dbname "example"})
+(def ds (jdbc/get-datasource db))
+(jdbc/execute! ds ["
+create table address (
+  id int auto_increment primary key,
+  name varchar(32),
+  email varchar(255)
+)"])
+
+(jdbc/execute! ds ["
+select * from address"])
+
+(jdbc/execute! ds ["INSERT INTO address (name, email) VALUES ('Per', 'Per@gmail.com')"])
