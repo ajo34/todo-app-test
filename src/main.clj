85eec541f772fa0d1809aa95f4cc3e4f763e36af
ajo34 <addavadda@gmail.com>
2025-04-10 14:43:07 +0200

    pedestal backend works

--- src/main.clj
+++ src/main.clj
@@ -1,18 +1,71 @@
-(ns main)
+(ns main
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
 
 (require '[next.jdbc :as jdbc] '[config :as config])
 ;(def db {:dbtype "h2" :dbname "example"})
 ;(def db "postgresql://localhost:5432/tododb")
 ;(clojure.core/refer 'config)
+(def ds (jdbc/get-datasource config/db-config))
+
+(defn ok [body]
+  {:status 200 :body body})
 
 
 
+(defn greeting-for [nm]
+  (println "name " nm)
+  (cond
+    (= nil nm) "Hello, person!\n"
+    (= "" nm) "Bad request"
+    :else (str "Hello, " nm "\n")))
 
+
+(defn respond-hello [request]
+  (let [nm (get-in request [:query-params :name])
+        resp (greeting-for nm)]
+    (ok resp)))
+
+(defn respond-fetch [req]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body (jdbc/execute! ds ["SELECT * FROM users"])})
+
+(map :users/username (jdbc/execute! ds ["SELECT * FROM users"]))
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]
+     ["/fet" :get respond-fetch :route-name :fet]}))
+
+(def service-map {::http/routes routes
+                  ::http/type :jetty
+                  ::http/port 8890})
+
+(defn start []
+  (http/start (http/create-server service-map)))
+
+
+;for interactive development
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server (assoc service-map
+                                                 ::http/join? false)))))
+(start-dev)
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
+(restart)
 
 
 ;jdbc stuff
 (comment
-  (def ds (jdbc/get-datasource config/db-config))
+
   (jdbc/execute! ds ["
                     CREATE TABLE users (
                     id SERIAL PRIMARY KEY,
